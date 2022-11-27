ca5d4ea9dc25a42addf4c4937a5ddb0de29b26b1
abdizamedmo <abdizamedmo@gmail.com>
2022-11-27 12:20:25 +0300

    blog posts list style and cols updated

--- components/Blogs/BlogPosts.tsx
+++ components/Blogs/BlogPosts.tsx
@@ -8,38 +8,37 @@ export default function BlogPosts({ posts }: any) {
       <h1 className="text-center my-3">
         List of Blogs <span className="badge bg-secondary">{posts.length}</span>
       </h1>
-      <div className="col-md-4 offset-md-2">
-        <div className="card-columns">
-          {posts.map((post: any) => (
-            <div
-              className="card rounded-0"
-              style={{
-                width: "20rem",
-              }}
-              key={post.id}
-            >
-              <Image
-                src={post.image ? post.image[0].url : "/images/default.jpg"}
-                alt={post.title}
-                width={300}
-                height={200}
-                className="card-img-top rounded-0"
-              />
+      {posts.map((post: any) => (
+        <div className="col-lg-3 col-md-6 col-sm-12" key={post.id}>
+          <div
+            className="card mb-5 rounded-0 shadow"
+            style={{
+              width: "20rem",
+            }}
+          >
+            <Image
+              src={post.image ? post.image[0].url : "/images/default.jpg"}
+              alt={post.title}
+              width={300}
+              height={200}
+              className="card-img-top rounded-0"
+            />
 
-              <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
-                <p className="card-text">{post.excerpt}</p>
+            <div className="card-body">
+              <h5 className="card-title">{post.title}</h5>
+              <p className="card-text limit-text">{post.excerpt}</p>
+              <div className="d-flex justify-content-end">
                 <Link
                   href={`/posts/${post.slug}`}
-                  className="btn btn-outline-primary"
+                  className=" btn btn-outline-primary"
                 >
                   Read more..
                 </Link>
               </div>
             </div>
-          ))}
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
